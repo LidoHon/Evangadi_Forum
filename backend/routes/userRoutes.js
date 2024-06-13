ad26f86e4e694031498bde037d9fa7690b9e8048
LidoHon <lidohon10@gmail.com>
2024-06-13 16:17:03 +0300

    first commit only back end

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import {
+	authUser,
+	registerUser,
+	logoutUser,
+	getUserProfile,
+	updateUserProfile,
+} from '../controllers/userControler.js';
+import { protect } from '../middleware/authMiddleware.js';
+const router = express.Router();
+
+router.post('/auth', authUser);
+router.post('/', registerUser);
+router.post('/logout', logoutUser);
+router
+	.route('/profile')
+	.get(protect, getUserProfile)
+	.put(protect, updateUserProfile);
+
+export default router;
---
+++ backend/server.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import userRoutes from './routes/userRoutes.js';
+import connectDB from './config/db.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import cookieParser from 'cookie-parser';
+dotenv.config();
+
+const port = process.env.PORT || 8000;
+connectDB();
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.json());
+app.use('/api/users', userRoutes);
+
+app.get('/', (req, res) => {
+	res.send('server is ready');
+});
+
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(port, () => {
+	console.log(`server running on port ${port}`);
+});
